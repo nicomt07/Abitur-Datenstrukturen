0cd9094ba90891c93839821c89ef7cb92c328ff8
Nico Tomas <nico.tomas07@gmail.com>
2026-04-18 21:59:52 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using VerketteteListe;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Siehe Formelsammlung: 4 Datenstrukturen\n\nStart\n= == = == = == = == = == = == = == = == = == =");
+        Test_Verkette_List();
+        Console.WriteLine("Sachen Hier");
+        Console.WriteLine("\n\n= == = == = == = == = == = == = == = == = == =\nEnde");
+    }
+    static void Test_Verkette_List()
+    {
+        var k1 = new Knoten<int>(0);
+        Console.WriteLine(k1.gibInhalt());
+    }
+    static void Test_Stapel()
+    {
+        ;
+    }
+    static void Test_Warteschlange()
+    {
+        ;
+    }
+    static void Test_Binaerbaum()
+    {
+        ;
+    }
+}
---
+++ VerketteteListe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VerketteteListe
+{
+    class Knoten<T>
+    {
+        public Knoten<T> naechster;
+        public T inhalt;
+        
+        public Knoten(T pinhalt)
+        {
+            this.inhalt = pinhalt;
+        }
+        public void setzeNeachsten(Knoten<T> pKn)
+        {
+            this.naechster = pKn;
+        }
+        public Knoten<T> gibNaechsten()
+        {
+            return this.naechster;
+        }
+        public void setzeInhalt(T pInhalt)
+        {
+            this.inhalt = pInhalt;
+        }
+        public T gibInhalt()
+        {
+            return this.inhalt;
+        }
+    }
+}
